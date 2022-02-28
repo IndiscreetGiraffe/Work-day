a9425c146fa7c29fce1208da1cc15c214cbbd19a
Kyleah Justice <kikij432@gmail.com>
2022-02-27 21:58:01 -0700

    added teh vars for the times in the script.js

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+var timeBlockNine = document.getElementById("hour9am");
+var timeBlockTen = document.getElementById("hour10am");
+var timeBlockEleven = document.getElementById("hour11am");
+var timeBlockTwelve = document.getElementById("hour12pm");
+var timeBlockOne = document.getElementById("hour1pm");
+var timeBlockTwo = document.getElementById("hour2pm");
+var timeBlockThree = document.getElementById("hour3pm");
+var timeBlockFour = document.getElementById("hour4pm");
+var timeBlockFive = document.getElementById("hour5pm");
+var textAreaEl = document.getElementsByClassName("time-block");
+
