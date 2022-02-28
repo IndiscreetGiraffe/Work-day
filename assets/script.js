591841c01633475e23502067a3c4bd1f80da1c66
Kyleah Justice <kikij432@gmail.com>
2022-02-27 22:09:42 -0700

    added more of the has to format and loop through elements to find class

--- assets/script.js
+++ assets/script.js
@@ -9,3 +9,33 @@ var timeBlockFour = document.getElementById("hour4pm");
 var timeBlockFive = document.getElementById("hour5pm");
 var textAreaEl = document.getElementsByClassName("time-block");
 
+// Add current day to top of calendar
+var currentDay = moment().format("ddd, MMM D ");
+var currentDayElement = document.getElementById("currentDay");
+currentDayElement.innerText = currentDay
+
+var currentTime = moment().format("H");
+
+var timeBlockElements = $(".textarea");
+
+    for (var i = 0; i< timeBlockElements.lenth; i++) {
+
+        var elementID = timeBlockElements[i].id;
+
+        var backgroundEl = document.getElementsById(timeBlockElements[i].id);
+
+        $(timeBlockElements[i].id).removeClass(".past .present .future");
+
+        if (elementID < currentTime) {
+            $(backgroundEl).addClass("past");
+        }
+        else if (elementID > currentTime) {
+            $(backgroundEl).addClass("future");
+        }
+        else {
+            $(backgroundEl).addClass("present");
+        }
+
+    }
+
+    
